d836dc99678553cafa5d7db6997fbc61c7e82bf1
Etienne Maheu <contact@etiennemaheu.com>
2015-01-19 00:55:02 -0500

    Updated the project to support CI generated versions

--- FluffIt.System.Reactive.WinPRT/Properties/AssemblyInfo.cs
+++ FluffIt.System.Reactive.WinPRT/Properties/AssemblyInfo.cs
@@ -32,6 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.9.9.9")]
+[assembly: AssemblyFileVersion("9.9.9.9")]
+[assembly: AssemblyInformationalVersion("9.9.9.9-alpha")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
--- FluffIt.System.Reactive/Properties/AssemblyInfo.cs
+++ FluffIt.System.Reactive/Properties/AssemblyInfo.cs
@@ -36,7 +36,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.9.9.9")]
+[assembly: AssemblyFileVersion("9.9.9.9")]
+[assembly: AssemblyInformationalVersion("9.9.9.9-alpha")]
 
 [assembly: InternalsVisibleTo("FluffIt.System.Reactive.Tests")]
